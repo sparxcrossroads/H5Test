baa38762616b054e8d59d429a28e2bf64d53bd7d
sparxcrossroads <kisshotblade@sina.com>
2015-08-30 23:22:07 +0800

    unity build bug
    PlayerLoop called recursively

---
+++ Assets/Script/GameDirection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+public class GameDirection : MonoBehaviour {
+
+    public void Build()
+    {
+        string[] levels = { "Assets/bui" };
+
+        BuildPipeline.BuildPlayer(levels,"WebPlayerBuild",
+                                BuildTarget.WebPlayer,BuildOptions.None);
+
+        Debug.Log("lucky");
+
+    }
+}
+   
