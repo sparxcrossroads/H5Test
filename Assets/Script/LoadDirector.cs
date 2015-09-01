1eb09bd623c9b59101204cb5f380689dcaddb000
sparxcrossroads <kisshotblade@sina.com>
2015-09-02 00:34:14 +0800

    场景分类
    场景过度,多场景集合 ,简单的文档编辑(没导出正确的格式)

---
+++ Assets/Script/LoadDirector.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadDirector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Application.LoadLevelAdditive("edit");
+        Debug.Log("ss");
+	}
+	
+}
