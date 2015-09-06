48bac5faff0dec3cdff221a76dcf708d8685e297
sparxcrossroads <kisshotblade@sina.com>
2015-09-07 01:02:25 +0800

    场景切换和 UI大小的布局
    默认 1136*640  场景切换顺序 edit->loading->show

---
+++ Assets/Script/EditDirector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditDirector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ToLoadingScene());
+	}
+
+    IEnumerator ToLoadingScene()
+    {
+        yield return null;
+        Application.LoadLevelAdditive("loading");
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
